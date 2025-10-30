773b8d90bf240a8f70c9ebd45842c41656d36b85
Khaled Bahjat <khaledbahjat0@gmail.com>
2025-10-30 12:01:02 -0700

    added firebase and init it

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
-
 import 'package:chat_app/core/routing/routing_generator.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main()=> runApp( ChatApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ChatApp());
+}
 
 class ChatApp extends StatelessWidget {
   const ChatApp({super.key});
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      
-      designSize: const Size(375,812),
+      designSize: const Size(375, 812),
       builder: (context, child) {
         return MaterialApp.router(
-          
           debugShowCheckedModeBanner: false,
           routerConfig: RoutingGenerator.goRouter,
         );
@@ -22,4 +27,3 @@ class ChatApp extends StatelessWidget {
     );
   }
 }
-
